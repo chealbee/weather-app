e8b5c717896ac56b47f301f569e66923fbed5dd0
Zahar Fartushniak <45snik45@gmail.com>
2022-07-23 12:02:08 +0300

    add base layoud

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import { FC } from "react";
+import AirIndex from "./components/airIndex/AirIndex";
+import DayCard from "./components/dayCard/DayCard";
+import SmoleCard from "./components/smoleCard/SmoleCard";
+import Button from "./components/UI/butoon/Button";
+import Input from "./components/UI/input/Input";
+import logo from "./img/logo.svg";
+import bgImage from "./img/coouds.jpg";
 
 const App: FC = () => {
    return (
       <>
-         <div className="container"></div>
+         <div className="container">
+            <div className="logo">
+               <img src={logo} alt="logo" />
+            </div>
+            <div className="search-section">
+               <Input placeholder="choose your city..." />
+               <Button text="search" type="search" />
+            </div>
+            <div className="main-section">
+               <DayCard />
+               <AirIndex />
+            </div>
+            <div className="slider">
+               <SmoleCard />
+               <SmoleCard />
+               <SmoleCard />
+            </div>
+         </div>
+         <img className="body-bg" src={bgImage} alt="bg-image" />
       </>
    );
 };
--- src/components/airIndex/AirIndex.tsx
+++ src/components/airIndex/AirIndex.tsx
@@ -4,38 +4,40 @@ import "./airIndex.scss";
 const AirIndex: FC = () => {
    return (
       <div className="air-card">
-         <div className="air-card__rigth">
-            <div className="air-card__head">
-               <h2 className="air-card__title">
-                  air index: <span>1</span>
-               </h2>
-               <p className="air-card__capture">air is good</p>
-            </div>
+         <div className="air-card__wraper">
+            <div className="air-card__rigth">
+               <div className="air-card__head">
+                  <h2 className="air-card__title">
+                     air index: <span>1</span>
+                  </h2>
+                  <p className="air-card__capture">air is good</p>
+               </div>
 
-            <div className="air-card__text">
-               The lower the index, the better.
+               <div className="air-card__text">
+                  The lower the index, the better.
+               </div>
             </div>
+            <ul className="air-card__side">
+               <li>
+                  co: <span>123</span>
+               </li>
+               <li>
+                  co: <span>123</span>
+               </li>
+               <li>
+                  co: <span>123</span>
+               </li>
+               <li>
+                  co: <span>123</span>
+               </li>
+               <li>
+                  co: <span>123</span>
+               </li>
+               <li>
+                  co: <span>123</span>
+               </li>
+            </ul>
          </div>
-         <ul className="air-card__side">
-            <li>
-               co: <span>123</span>
-            </li>
-            <li>
-               co: <span>123</span>
-            </li>
-            <li>
-               co: <span>123</span>
-            </li>
-            <li>
-               co: <span>123</span>
-            </li>
-            <li>
-               co: <span>123</span>
-            </li>
-            <li>
-               co: <span>123</span>
-            </li>
-         </ul>
       </div>
    );
 };
